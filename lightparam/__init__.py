6281f8ff08db7886edb0abbb81a564c97b06da30
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-07-26 00:20:28 +0200

    Expose again in init set_nested and get_nested

--- lightparam/__init__.py
+++ lightparam/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "0.4.4"
 __author__ = "Vilim Stich, Luigi Petrucco",
 
-from lightparam.core import Param, ParamContainer, ParameterTree, Parametrized
+from lightparam.core import Param, ParamContainer, ParameterTree, \
+    Parametrized, get_nested, set_nested
